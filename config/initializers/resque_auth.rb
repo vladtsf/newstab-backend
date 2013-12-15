bb232e9674754e0a95807e2c3e2465cd6f10e5df
Vladimir Tsvang <vtsvang@gmail.com>
2013-12-15 16:52:57 +0200

    Fixed resque_auth

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,6 +1,6 @@
 auth_credentials = YAML.load_file("#{Rails.root}/config/resque_auth.yml")[Rails.env].symbolize_keys
 
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  user == auth_credentials[:user]
-  password == auth_credentials[:password]
+  user == auth_credentials[:user].to_s
+  password == auth_credentials[:password].to_s
 end
